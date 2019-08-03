3500926a6bafa0e8cd4c199da20d5f2c47aecbee
zyzisyz <zyziszy@foxmail.com>
2019-08-03 23:36:10 +0800

    update

---
+++ 2-Angular/data/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# *************************************************************************
+#	> File Name: __init__.py
+#	> Author: Yang Zhang 
+#	> Mail: zyziszy@foxmail.com
+#	> Created Time: Sat 03 Aug 2019 11:23:39 PM CST
+# ************************************************************************/
+
+
+
